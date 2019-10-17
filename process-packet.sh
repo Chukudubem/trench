12524ce1ba37b175930c8c585d2a480e65a4fe62
Dubem Nwoji <nwoji@usc.edu>
2019-10-17 11:34:25 -0700

    Update process-packet.sh

--- process-packet.sh
+++ process-packet.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
-#Include
+echo "Welcome to Trench!" | figlet
+echo $(date -u)
+echo "========== =========== ========== ==========="
 
-'''
-mkdir -p temp/negs/
-mkdir -p temp/poss/
-FILES=$(find /mnt/hgfs/medium/Benign/ -type f -name '*.pcap')
-FILES2=$(find /mnt/hgfs/medium/Adware/ -type f -name '*.pcap')
+#Create folders
+mkdir -p temp/neg/
+mkdir -p temp/pos/
 
+#Set file paths
+FILES=$(find <path/to/folder/for/benign/> -type f -name '*.pcap')
+FILES2=$(find <path/to/folder/for/malware/> -type f -name '*.pcap')
+
+#Process benign traffic
 for filename in $FILES; do
     input_path="$filename"
     output_path="${input_path%.pcap}.gz"
     echo "Processing ${input_path##*/}"
-    ../joy/bin/joy bidir=1 tls=1 dist=1 "$input_path" > "temp/negs/${output_path##*/}"
+    ../joy/bin/joy bidir=1 tls=1 dist=1 "$input_path" > "temp/neg/${output_path##*/}"
 done
 
+#Process malware traffic
 for filename in $FILES2; do
     input_path="$filename"
     output_path="${input_path%.pcap}.gz"
     echo "Processing ${input_path##*/}"
-    ../joy/bin/joy bidir=1 tls=1 dist=1 "$input_path" > "temp/poss/${output_path##*/}"
+    ../joy/bin/joy bidir=1 tls=1 dist=1 "$input_path" > "temp/pos/${output_path##*/}"
 done
+
 
 echo "================================================================================="
 echo "Done Processing files"
 echo "================================================================================="
 echo " "
-echo " "
 
-#input_path="$1"
-#output_path="${input_path%.pcap}.gz"
-
-
-
-#../joy/bin/joy bidir=1 tls=1 dist=1 "$input_path" > "temp/${output_path}"
-'''
-
-#echo "$output_path" 
+echo "================================================================================" 
 echo "Running Python Script"
-python trench.py temp/poss/ temp/negs/
+echo "-------------------------------------------------------------------------------"
+
+python trench.py temp/pos/ temp/neg/
