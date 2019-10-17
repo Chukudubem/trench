dbb4395ceb49d057a5a6b16cfbf79e9709af0dd8
Dubem Nwoji <nwoji@usc.edu>
2019-10-17 11:49:15 -0700

    Update trench.py

--- trench.py
+++ trench.py
@@ -1,12 +1,27 @@
 import os
 import sys
+
+#If Location(ETL, Classifier, DataProcessor) != Current Location; do "sys.path.insert(0, <path/to/script>)"
+
 from ETL import ETL
 from Classifier import LogisticRegressor
 from DataProcessor import DataProcessor as processor
 
-sys.path.insert(0, '/mnt/hgfs/medium/scripts/')
+#Pass argument to python script
+pos_file = sys.argv[1]
+neg_file = sys.argv[2]
 
-output_file = sys.argv[1]
+#Load/Process Data
+print("Loading Data...")
+X_pos, y_pos = ETL().load_data(file_path=pos_file, label=1)
+X_neg, y_neg = ETL().load_data(file_path=neg_file, label=0)
+print("Done Loading data")
 
-X_pos, _ = ETL().load_data(file_path=output_file, label=1)
-print(len(X_pos))
+#Concatenate data
+X_pos.extend(X_neg)
+y_pos.extend(y_neg)
+
+X = X_pos
+y = y_pos
+
+
